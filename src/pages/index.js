b992b7c8b282c5162576884d34f53b28146e89d3
wuiancocos2d <wuiancocos2d@gmail.com>
2018-06-02 23:52:12 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from '../componets/navbar/navbar';
+
+
+class Index extends Component {
+    render(){
+        return (
+            <Navbar />
+        );
+    }
+}
+
+export default Index;
