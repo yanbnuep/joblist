097247270517eb6564fb7495276461c4be5e4b25
yanbnuep <yanbnuep@gmail.com>
2018-06-03 18:03:46 +0800

    logo

--- src/componets/navbar/navbar.js
+++ src/componets/navbar/navbar.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import { Menu } from 'antd';
+import React, {Component} from 'react';
+import {Menu, Row, Col} from 'antd';
+import logo from '../../assets/nxlogo170_48.png';
+
 class Navbar extends Component {
     render() {
         return (
-            <Menu theme="dark"
-            mode="horizontal"
-            style={{lineHeight: '64px'}}
-            >
-                <Menu.Item key="1">nav 1</Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>   
-            </Menu>
+            <Row>
+                <Col xs={24} sm={24} md={6} lg={5} xl={4}>
+                    <a href="/" id="logo">
+                        <img alt="logo" src={logo}/>
+                    </a>
+                </Col>
+                <Col xs={0} sm={0} md={18} lg={19} xl={20}>
+                <Menu theme="dark"
+                      mode="horizontal"
+                      style={{lineHeight: '64px'}}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Col>
+            </Row>
         );
     }
 }
