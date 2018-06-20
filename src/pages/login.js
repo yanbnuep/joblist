413c9ab85172470fd847cb87309e3b2d8833f570
yanbnuep <yanbnuep@gmail.com>
2018-06-20 21:05:53 +0800

    detail

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,18 +3,18 @@ import React from 'react';
 import axios from 'axios';
 import qs from 'qs';
 import { Form, Input, Icon, Button } from 'antd';
-require('../componets/mock/login_mock');
+// require('../componets/mock/login_mock');
 const FormItem = Form.Item;
 class Login extends React.Component {
     handleSubmit = function(e){
         e.preventDefault();
         axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
-        axios.post('http://app.airmacau.com.mo/etl/api/login.ashx',qs.stringify({uid:'04035',pwd:'04035pwd'})).then(
+        axios.post('http://app.airmacau.com.mo/itlog/index.aspx',qs.stringify({uid:'04035',pwd:'0405pwd'})).then(
             function(response){
                 console.log(response);
                 const sid = response.data.SID;
                 if(sid){
-                    console.log(sid);
+                    console.log()
                 }
             }
         ).catch(function(error){
